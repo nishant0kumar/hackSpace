863d925292c631e2c1c727ec012287008d1ec8a5
Nishant kumar <nishant51121@gmail.com>
2024-05-20 18:47:01 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,35 @@
+import '../assets/footer.css'
+import logo from '../../public/logo.svg'
+
 function Footer() {
+
+    let coded = `while (idea!= null ){
+        work = True;
+        result = noTension()
+        if (work == result){
+            return keepGoing;
+        }
+        else{
+            idea = new();
+        }
+    }`
     return(
         <div>
             <footer>
-                <div class="code">
+                <div className="footer-logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="code">
                     <code>
-                        
+                        {coded}
                     </code>
                 </div>
             </footer>
-            <div class="copy">
+            <div className="copy">
                 <p>allCopyRightReserved to (c) hackSpace build with love</p>
             </div>
         </div>
     );
 }
+
+export default Footer;
